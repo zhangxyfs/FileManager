2f824a5de155c31afa8ad141a6485773e588e23b
zhangxiaoyu <zhangxiaoyu@123shishi.com>
2017-07-28 17:38:31 +0800

    增加一个checkbox

---
+++ manager/src/main/java/com/z7dream/manager/tool/smoothCompound/MaterialColor.java
@@ -0,0 +1,26 @@
+package com.z7dream.manager.tool.smoothCompound;
+
+import com.eblog.lib.Appli;
+import com.eblog.lib.R;
+
+public class MaterialColor {
+
+    public static final int material_deep_teal_500 = 0xff009688;
+
+    //搬运自themes_material.java中的Theme.Material.Light
+    public static final class DefaultLight {
+        //我是分割线
+        public static final int colorPrimaryDark = Appli.getContext().getResources().getColor(R.color.colorPrimaryDark);//0xff757575;//@color/primary_dark_material_light
+        public static final int colorPrimary = Appli.getContext().getResources().getColor(R.color.colorPrimary);//0xffbdbdbd;//@color/primary_material_light
+        public static final int colorAccent = Appli.getContext().getResources().getColor(R.color.color_white);//material_deep_teal_500;//@color/accent_material_light
+        //我是分割线
+        //public static final int colorControlNormal = 0;//?attr/textColorSecondary
+        //public static final int colorControlActivated = 0;//?attr/colorAccent
+        //我是分割线
+        public static final int colorControlHighlight = 0x40000000;//@color/ripple_material_light
+        //public static final int colorButtonNormal = 0;//@color/btn_default_material_light
+        //public static final int colorSwitchThumbNormal = 0;//@color/switch_thumb_material_light
+
+    }
+
+}
